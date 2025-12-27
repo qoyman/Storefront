9a745123b8bf9ab7e755c026a016bdd3d5997f75
qoyman <isholakareem28@gmail.com>
2025-12-27 07:14:36 +0100

    storefront app

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request, 'hello.html',{'name':'Qoyyum'})
+
+# Create your views here.
